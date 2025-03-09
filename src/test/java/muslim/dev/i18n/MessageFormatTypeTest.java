21cd16aeac50a9684c54f5f2948e31ea3ee78568
herman-xphp <hermanxphp@gmail.com>
2025-03-09 23:53:24 +0800

    Message Format Type

---
+++ src/test/java/muslim/dev/i18n/MessageFormatTypeTest.java
@@ -0,0 +1,26 @@
+package muslim.dev.i18n;
+
+import java.text.MessageFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.junit.jupiter.api.Test;
+
+public class MessageFormatTypeTest {
+
+  @Test
+  void testMessageFormatType() {
+    Locale locale = new Locale("in", "ID");
+    ResourceBundle resourceBundle = ResourceBundle.getBundle("message", locale);
+
+    String pattern = resourceBundle.getString("status");
+
+    MessageFormat messageFormat = new MessageFormat(pattern, locale);
+    String format = messageFormat.format(new Object[] {
+        "Herman", new Date(), 1_000_000
+    });
+
+    System.out.println(format);
+  }
+}
